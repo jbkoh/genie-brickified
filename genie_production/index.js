c76a63fb6c0d47c5d108911993d548f97c40b4ee
Renxu Hu <reh011@ucsd.edu>
2019-06-28 04:06:21 -0700

    Added the script for production

---
+++ genie_production/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const https = require("https");
+const httpProxy = require('http-proxy');
+const fs = require("fs");
+
+const options = {
+    cert: fs.readFileSync('/home/renxu/fullchain.pem'),
+    key: fs.readFileSync('/home/renxu/privkey.pem')
+};
+const proxy_url = 'https://0.0.0.0:5000'
+
+const app = express();
+const proxy = httpProxy.createProxyServer({});
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, '/build')));
+
+app.all(/^\/api\/(.*)/, (req, res) => {
+    proxy.web(req, res, { target: proxy_url });
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
+const port = 11001;
+https.createServer(options, app).listen(port);
+
+console.log('App is listening on port ' + port);
